d9c821d5fd60a37b671979cf9a6c60c093d875bc
smhrd <smhrd@61.80.80.187>
2022-05-31 16:07:15 +0900

    메인화면입니다!!

---
+++ teamgg/src/miniproject/MafiaGameMain.java
@@ -0,0 +1,12 @@
+package miniproject;
+
+public class MafiaGameMain {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("마피아게임 메인 화면입니다!!!");
+		System.out.println("다들 보이시나요?");
+	}
+
+}
